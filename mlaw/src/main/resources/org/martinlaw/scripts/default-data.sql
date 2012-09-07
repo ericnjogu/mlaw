ad7d2b17bf1b13d9e430dadf151cd79d7658469a
mugo <kunadawa@gmail.com>
2012-09-07 15:23:40 +0300

    added object ids in an attempt to troubleshoot one to one relationship fetch inconsistency

--- mlaw/src/main/resources/org/martinlaw/scripts/default-data.sql
+++ mlaw/src/main/resources/org/martinlaw/scripts/default-data.sql
@@ -1,7 +1,9 @@
 
 insert into martinlaw_status_t 
-(status_id, status, type) 
+(status_id, status, type, ver_nbr,  obj_id) 
 values 
-(1001, 'pending', "ANY_TYPE"), (1002, 'hearing', "COURT_CASE_TYPE"), 
-(1003, 'closed', "ANY_TYPE"), (1004, 'documents missing', "CONVEYANCE_TYPE"),
-(1005, 'adjourned', "COURT_CASE_TYPE");
+(1001, 'pending', "ANY_TYPE", 1, 'rVFEm1NEx898d3LVZ5wpM3NoGj4fc5odHitf'), 
+(1002, 'hearing', "COURT_CASE_TYPE", 1, 'urJMtBzP7nGFkGeCxW6yIUe3TBBO2qyUNpjC'), 
+(1003, 'closed', "ANY_TYPE", 1, '19ArvTXtirtmHRbmE5mO13R1rGvwej1zO94H'), 
+(1004, 'documents missing', "CONVEYANCE_TYPE", 1, 'TrUJbOnoGZguzwZerTVa5Ec2rZQQlvra9jQv'),
+(1005, 'adjourned', "COURT_CASE_TYPE", 1, 'pC91h1ZSYcAdhAXIzjotJIMoowo0ToqcJFGV');
