67ce19df3b370eba028e975347f30860afb223bc
mugo <kunadawa@gmail.com>
2012-10-18 14:57:48 +0300

    now checking whether document initiator has been assigned to the matter

--- mlaw/src/main/java/org/martinlaw/bo/MatterWorkRule.java
+++ mlaw/src/main/java/org/martinlaw/bo/MatterWorkRule.java
@@ -4,8 +4,10 @@
 package org.martinlaw.bo;
 
 import org.kuali.rice.core.api.util.RiceKeyConstants;
+import org.kuali.rice.kim.api.services.KimApiServiceLocator;
 import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.krad.rules.TransactionalDocumentRuleBase;
+import org.kuali.rice.krad.service.KRADServiceLocator;
 import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
 import org.kuali.rice.krad.util.ErrorMessage;
 import org.kuali.rice.krad.util.GlobalVariables;
@@ -27,16 +29,59 @@ public class MatterWorkRule extends TransactionalDocumentRuleBase {
 	public boolean processCustomSaveDocumentBusinessRules(Document document) {
 		MatterWork matterWork = (MatterWork) document;
 		if (matterWork.isMatterIdValid()) {
-			return true;
+			// check if the initiator is an assignee for this matter
+			String principalId = document.getDocumentHeader().getWorkflowDocument().getInitiatorPrincipalId();
+			String initiatorPrincipalName = KimApiServiceLocator.getIdentityService().getPrincipal(principalId).getPrincipalName();
+			if (isPrincipalNameInAssigneeList(matterWork, initiatorPrincipalName)) {
+				return true;
+			} else {
+				ErrorMessage errMsg = new ErrorMessage(Constants.MessageKeys.ERROR_NOT_INITIATOR, initiatorPrincipalName, 
+								matterWork.getMatterClass().getSimpleName());
+				addMatterIdError(errMsg);
+				return false;
+			}
 		} else {
-			GlobalVariables.getMessageMap().addToErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
 			ErrorMessage errMsg = new ErrorMessage(RiceKeyConstants.ERROR_EXISTENCE, 
 					KRADServiceLocatorWeb.getDataDictionaryService().getAttributeLabel(
 							matterWork.getMatterClass(), Constants.PropertyNames.MATTER_ID));
-			GlobalVariables.getMessageMap().putError(Constants.PropertyNames.MATTER_ID, errMsg);
-			GlobalVariables.getMessageMap().removeFromErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
+			addMatterIdError(errMsg);
 			
 			return false;
 		}
+	}
+
+	/**
+	 * convenience method to add an error relating to the matter id to the global variables
+	 * 
+	 * @param matterWork
+	 */
+	protected void addMatterIdError(ErrorMessage errMsg) {
+		GlobalVariables.getMessageMap().addToErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
+		GlobalVariables.getMessageMap().putError(Constants.PropertyNames.MATTER_ID, errMsg);
+		GlobalVariables.getMessageMap().removeFromErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
+	}
+	
+	/**
+	 * determines if a principal name is in the assignee list
+	 * 
+	 * @param matterWork - the work document
+	 * @param principalName - the principal name to look for
+	 * 
+	 * @return true if found, false if not or the list is empty
+	 */
+	public boolean isPrincipalNameInAssigneeList(MatterWork matterWork, String principalName) {
+		Matter<? extends MatterAssignee, ? extends MatterWork> matter = KRADServiceLocator.getBusinessObjectService().findBySinglePrimaryKey(
+				matterWork.getMatterClass(), matterWork.getMatterId());
+		if (matter == null || matter.getAssignees() == null || matter.getAssignees().size() == 0) {
+			return false;
+		} else {
+			for (MatterAssignee assignee: matter.getAssignees()) {
+				if (assignee.getPrincipalName().equalsIgnoreCase(principalName)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
