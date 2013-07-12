62966c5afceb8fe60980764f1682c71465f1c80a
mugo <kunadawa@gmail.com>
2013-07-12 13:14:40 +0300

    renamed method

--- mlaw/src/test/java/org/martinlaw/test/type/BaseDetailRoutingTestBase.java
+++ mlaw/src/test/java/org/martinlaw/test/type/BaseDetailRoutingTestBase.java
@@ -83,7 +83,7 @@ public abstract class BaseDetailRoutingTestBase extends KewTestsBase implements 
 	}
 
 	@Test
-	public void testContractTypeMaintDocPerms() {
+	public void testBaseDetailMaintDocPerms() {
 		testCreateMaintain(getDataObjectClass(), getDocTypeName());
 	}
 
